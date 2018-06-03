91170d2276c4f1a71b2388bbb02e36ef4de5db05
yoinkbird <YoinkBird2010@gmail.com>
2018-06-03 10:59:49 -0500

    update: geod_lut - first progress

--- code/jsonlib.py
+++ code/jsonlib.py
@@ -4,6 +4,7 @@
 import json
 import pprint as pp
 import re
+import os,sys
 
 # hard-coded globals
 resource_dir = "output"
@@ -27,8 +28,10 @@ if __name__ == '__main__':
     options['verbose'] = args.verbose
 
 
-def print_test():
-    print("hi")
+def print_test(*args):
+    print("####################")
+    print("SELF-TEST: %s" % args)
+    print("--------------------")
     return("hi")
 
 # open and return loaded json file
@@ -66,16 +69,72 @@ def save_json_file(response_json, filename, **options):
 
     return filename
 
+def convert_block_groups_geojson_to_geoid_lut(geoid_data,**options):
+    '''
+    Purpose: generate LUT (look up table) of GEOIDs => polygons based on the block-groups.geojson file
+
+    # Format:
+     {
+     "type": "FeatureCollection",
+     "crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+     "features": [
+         { "type": "Feature",
+           "properties":
+               { "GEOID": "480139604021",
+               #...
+               }, 
+      { 
+        "type": "Polygon", 
+        "coordinates": 
+        [ [ 
+          [ -98.484237, 28.957106 ], 
+               #...
+        ] ] 
+       }, 
+
+    i.e.
+    geoidval = data['features'][0][properties][GEOID]
+    polygon  = data['features'][0]['geometry']['coordinates']
+    '''
+    lut_dict = {}
+    for index, feature in enumerate( geoid_data['features'] ):
+        geoidval = feature['properties']['GEOID']
+        if ( feature['geometry']['type'] == "Polygon" ):
+            lut_dict[ geoidval ] = {}
+            lut_dict[ geoidval ]['geometry'] = {}
+            lut_dict[ geoidval ]['geometry'] = feature['geometry']
+        else:
+            print("ERROR: missing entry Polygon on line %s" % (index))
+            print(json.dumps( feature ))
+            system.exit()
+    return lut_dict
+
+
+
 if(__name__ == '__main__'):
     #print_test()
     input_json_path="res/samples" + "/" + "geojson_blockgroup_lut.json"
     output_json_path="t/tmp/jsontest" + "/" + "out.json"
     in_json = retrieve_json_file(input_json_path, **options)
-    print("SELF-TEST: see the loaded json:")
+    print_test("see the loaded json:")
     print(json.dumps(in_json))
-    print("SELF-TEST: dumping to file the json:")
+    print_test("dumping to file the json:")
     save_json_file(in_json, output_json_path, **options)
     # verify stored by loading and dumping
     print(json.dumps(
             retrieve_json_file(output_json_path, **options)
         ))
+
+
+    print_test("geoid LUT")
+
+    geoid_path="res/samples"
+    geoid_path=geoid_path + "/" "block-groups_firstentry.geojson"
+    print(json.dumps(
+        convert_block_groups_geojson_to_geoid_lut(
+            retrieve_json_file(geoid_path, **options), **options)
+        ))
+    # pp.pprint(
+    #         convert_block_groups_geojson_to_geoid_lut(
+    #         retrieve_json_file(geoid_path, **options), **options)
+    #             )
